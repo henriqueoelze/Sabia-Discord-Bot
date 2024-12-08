579248b38708ec9f8eec96bed11a2882d9789f27
Henrique Oelze <henriqueoelze@gmail.com>
2024-12-07 22:18:11 -0300

    Adding log as env var

--- main.py
+++ main.py
@@ -20,11 +20,12 @@ async def load_cogs():
 
 async def main():
     BOT_TOKEN = os.getenv('BOT_TOKEN')
+    LOG_LEVEL = os.getenv('LOG_LEVEL') if os.getenv('LOG_LEVEL') is not None else 'INFO'
 
     # Log setup
     logging.basicConfig(level=logging.INFO)
     logger = logging.getLogger('discord')
-    logger.setLevel(logging.DEBUG)
+    logger.setLevel(logging.getLevelName(LOG_LEVEL))
     handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
     handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
     logger.addHandler(handler)
