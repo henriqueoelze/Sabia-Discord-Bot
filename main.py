ab548bbf52fa85f92e82e624e625a0ff5279fd7b
Henrique Oelze <henriqueoelze@gmail.com>
2024-12-07 22:14:33 -0300

    Adding log setup

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
+import logging
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -12,12 +13,22 @@ bot_prefix = "/"
 bot = commands.Bot(intents=intents)
 
 
+
 async def load_cogs():
     bot.load_extension('cogs.proxy')
 
 
 async def main():
     BOT_TOKEN = os.getenv('BOT_TOKEN')
+
+    # Log setup
+    logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger('discord')
+    logger.setLevel(logging.DEBUG)
+    handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+    handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+    logger.addHandler(handler)
+
     await load_cogs()
     await bot.start(BOT_TOKEN)
 
